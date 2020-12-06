ca27aca231af2005cb71b7d280e950f3358a7fee
gero1992 <41724817+gero1992@users.noreply.github.com>
2020-12-06 01:54:46 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,25 @@
-
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
-import filesize from 'rollup-plugin-filesize';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
+import common from '@rollup/plugin-commonjs';
 import copy from 'rollup-plugin-copy';
-import common from 'rollup-plugin-commonjs';
 
-
-const filesizeConfig = {
-    showGzippedSize: true,
-    showBrotliSize: false,
-    showMinifiedSize: false,
-};
-
-const copyConfig = {
+const targets = {
     targets: [
-        { src: ['node_modules/@webcomponents', 'node_modules/@polymer'], dest: 'dist/node_modules' },
-        { src: 'index.html', dest: 'dist', rename: 'index.html' },
-    ],
-};
-
-// The main JavaScript bundle for modern browsers that support
-// JavaScript modules and other ES2015+ features.
-const config = {
-    input: ['side-menu.js', "side-menu-item.js", "side-menu-item-style.js"],
-    output: {
-        dir: 'dist',
-        format: 'es',
-    },
-
-    plugins: [
-        minifyHTML(),
-        common(),
-        copy(copyConfig),
-        resolve(),
-        filesize(filesizeConfig)
-    ],
-    preserveEntrySignatures: false,
-};
-
-if (process.env.NODE_ENV !== 'development') {
-    config.plugins.push(terser());
+        { src: ['node_modules/@webcomponents', 'node_modules/@polymer'], dest: 'demo/node_modules' },
+        { src: 'index.html', dest: 'demo', rename: 'index.html' },
+    ]
 }
 
-export default config;
+export default {
+    input: ['side-menu.js', "side-menu-item.js", "side-menu-item-style.js"],
+    output: {
+        dir: 'demo',
+        format: 'esm'
+    },
+    plugins: [
+        common({
+            include: /node_modules/,
+        }),
+        copy(targets),
+        resolve()
+    ]
+};
